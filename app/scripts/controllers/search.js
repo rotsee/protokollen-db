d0c79685a8510541dbf32daabaef0bd0bec9c5e4
jensfinnas <jens.finnas@gmail.com>
2014-12-21 20:50:28 +0200

    Fix date range

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -8,37 +8,32 @@
  * Controller of the protokollenApp
  */
 
-
+// SERVICE: Store the current search parameters (dates, keyword etc) centrally
+// so that various controllers on the page can access them
 angular.module('protokollenApp')
 	.factory('currentQuery', function($rootScope) {
 		var service = {};
 		service.values = {};
+
+		// Update parameter
 		service.set = function(key, value) {
 			service.values[key] = value;
 			$rootScope.$broadcast("valuesUpdated", key, value);
 		}
+
+		// Request a specific parameter
 		service.get = function(key) {
 			return service.values[key];
 		};
 		return service;
 	});
+
+// The search controller picks up route parameters and inits the 
+// currentQuery service
 angular.module('protokollenApp').controller('SearchCtrl', ['$scope', '$routeParams', 'currentQuery',
 		function ($scope, $routeParams, currentQuery) {
 			// Get search parameters from route or use default values
 			currentQuery.set('from', $routeParams.from || '2010-01-01');
 			currentQuery.set('to', $routeParams.to || '2015-01-01');
-/*			$scope.daterange = {
-				from: currentQuery.get('from'),
-				to: currentQuery.get('to')
-			};
-			$scope.$watch('daterange.from', function(newValue) {
-				console.log(newValue);
-				currentQuery.set('from', $scope.daterange.from);
-			})
-			$scope.$on('valuesUpdated', function() {
-				console.log("update values");
-				$scope.daterange.from = currentQuery.from;
-				$scope.daterange.to = currentQuery.to;
-			})*/
 		}
 	]);
--- app/scripts/controllers/ui.bootstrap.datepicker.ctrl.js
+++ app/scripts/controllers/ui.bootstrap.datepicker.ctrl.js
@@ -1,7 +1,16 @@
 angular.module('protokollenApp')
+// The date range controller stores a date range for filtering
 .controller('DaterangeCtrl', function ($scope, currentQuery) {
+    $scope.daterange = {};
+    $scope.daterange.from = currentQuery.get('from');
+    $scope.daterange.to = currentQuery.get('to');
 
+    // currentQuery will signal when dates are updated 
+    $scope.$on('valuesUpdated', function($parent, key) {
+      $scope.daterange[key] = currentQuery.get(key);
+    })
 })
+// Controller for the current search url element
 .controller('SearchUrlCtrl', function ($scope, currentQuery) {
   $scope.$on('valuesUpdated', function($parent, key) {
     $scope.from = currentQuery.get('from');
@@ -9,24 +18,24 @@ angular.module('protokollenApp')
   });
 });
 
+// The controller of a single datepicker (from Bootstrap UI)
 angular.module('protokollenApp')
 .controller('DatepickerCtrl', function ($scope, $attrs, currentQuery) {
   // Define a custom model, eg. "from" or "to"
   var key = $attrs.key || 'dt';
   $scope.daterange = {};
-  // The starting date from parent from/to or set current date as default
+
+  // Get starting dates from currentQuery service servie
   $scope.daterange[key] = currentQuery.get(key) || new Date();
 
+  // Update currentQuery service when field changes
   var keyString = 'daterange.' + key;
   $scope.$watch(keyString, function() {
     currentQuery.set(key, $scope.daterange[key]);
+    $scope.daterange[key]
   }, true);
 
-  $scope.$on('valuesUpdated', function($parent, key) {
-    $scope.daterange[key] = currentQuery.get(key);
-  })
-
-
+  // Get current date
   $scope.today = function() {
     $scope.daterange[key] = new Date();
   };
