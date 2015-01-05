b584a99987848ea5e5b9dd23d0e4b464631af384
jensfinnas <jens.finnas@gmail.com>
2015-01-05 13:16:43 +0100

    Sorting

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -36,6 +36,8 @@ angular.module('protokollenApp').controller('SearchCtrl', ['$scope', '$routePara
 		currentQuery.set('from', $routeParams.from || '2010-01-01');
 		currentQuery.set('to', $routeParams.to || '2015-01-01');
 		currentQuery.set('keyword', $routeParams.keyword || '');
-		$scope.foo = 'bar';
+		$scope.sort = function(order) {
+			this.sorting.sort = ejs.Sort('meeting_date').order(order);
+		};
 	}
 ]);
